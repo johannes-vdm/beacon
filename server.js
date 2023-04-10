9320ff9244421c6f2cbc7077491265734000c3d0
Johannes van der Merwe <johannes.vandermerwe@adaptit.com>
2023-04-10 11:52:02 +0200

    socket alternative

--- server.js
+++ server.js
@@ -1,101 +1,30 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
 
-// Set a 30-minute timeout for the response
-const TIMEOUT = 30 * 60 * 1000; // 30 minutes in milliseconds
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Keep track of connected clients
-const clients = new Set();
-
-// Initialize watchtowerData to an empty object
-app.locals.watchtowerData = {};
-
-// Endpoint for receiving data from Python server
-app.post('/api/forward', (req, res) => {
-  let data = '';
-  req.on('data', chunk => {
-    data += chunk;
-  });
-
-  req.on('end', () => {
-    // Save the data to be sent to connected clients
-    app.locals.watchtowerData = JSON.parse(data);
-    // Send a 200 OK response
-    res.sendStatus(200);
-  });
+http.listen(3000, () => {
+  console.log('Server listening on port 3000...');
 });
 
-// Endpoint for serving the HTML page with the latest JSON response
+let latestResponse = 'No Response';
+
+app.post('/api/watchtower', (req, res) => {
+  const responseData = req.body;
+  latestResponse = responseData;
+  res.send('Response received');
+});
+
 app.get('/', (req, res) => {
-  const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Beacon App</title>
-      </head>
-      <style>
-        body {
-          background-color: black;
-          color: white;
-          font-family: 'Courier New', Courier, monospace;
-        }
-      </style>
-      <body>
-        <h1 id="data"></h1>
-        <script>
-          const source = new EventSource('/watchtower');
-          const pre = document.getElementById('data');
-          source.addEventListener('message', event => {
-            pre.textContent = event.data;
-          });
-        </script>
-      </body>
-    </html>
-  `;
-  res.send(html);
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Endpoint for serving the data received from Python server as server-sent events
-app.get('/watchtower', (req, res) => {
-  // Set the headers for server-sent events
-  res.set({
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
+io.on('connection', (socket) => {
+  console.log('Socket connection established');
 
-  // Add the client to the set of connected clients
-  clients.add(res);
-
-  // Send the current data to the client
-  if (app.locals.watchtowerData) {
-    res.write(`data: ${JSON.stringify(app.locals.watchtowerData)}\n\n`);
-  }
-
-  // Remove the client from the set of connected clients when the connection is closed
-  req.on('close', () => {
-    clients.delete(res);
-  });
+  socket.emit('latestResponse', latestResponse);
 });
-
-// Start the server
-app.listen(process.env.PORT || 3000, () => console.log('Watchtower app listening on port!'));
-
-// Set a 30-minute timeout for the response
-app.use((req, res, next) => {
-  res.setTimeout(TIMEOUT, () => {
-    // Send a 404 Not Found response if the timeout expires
-    res.sendStatus(404);
-  });
-  next();
-});
-
-// Send the current data to all connected clients every second
-setInterval(() => {
-  if (app.locals.watchtowerData) {
-    clients.forEach(client => {
-      client.write(`data: ${String(app.locals.watchtowerData.data)}\n\n`);
-    });
-  }
-}, 1000);
