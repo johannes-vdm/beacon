7829455a14194774a2540d8f85f70a452fa601a8
Johannes van der Merwe <johannes.vandermerwe@adaptit.com>
2023-04-10 11:59:17 +0200

    public folder

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
 
+// Set up the middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
-http.listen(3000, () => {
-  console.log('Server listening on port 3000...');
+// Set up the server to listen on the port provided by Heroku or 3000
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
 });
 
+// Set up a variable to store the latest response
 let latestResponse = 'No Response';
 
+// Set up a route to handle post requests to /api/watchtower
 app.post('/api/watchtower', (req, res) => {
   const responseData = req.body;
   latestResponse = responseData;
+  io.emit('latestResponse', latestResponse); // Push the latest response to all connected clients
   res.send('Response received');
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
+// Set up Socket.io to update the HTML page without refreshing
 io.on('connection', (socket) => {
   console.log('Socket connection established');
 
+  // Send the latest response to the client on connection
   socket.emit('latestResponse', latestResponse);
 });
